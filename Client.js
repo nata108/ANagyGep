4117c898bc0f1854596e59281222725d28122bf5
nata108 <natabara@gyongyossy.hu>
2021-11-06 22:10:14 +0100

    Communicator

---
+++ Client.js
@@ -0,0 +1,35 @@
+import Websocket from "ws";
+import readline from "readline";
+
+
+
+var myArgs = process.argv.slice(2)
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+const readsend = ()=>{
+    var d = new Date();
+    rl.question(d.getTime().toString()+" ["+myArgs[1]+"-->]: ",function (answer) {
+        client.send(answer);
+        readsend();
+    });
+}
+
+const client = new Websocket("ws:"+myArgs[0]+":7071");
+client.on('open', ()=>{console.log("Connected to NagyGép!")
+    client.send(myArgs[1])
+readsend()
+});
+client.on('close',()=>{console.log("\nDisconnecting from NagyGép!")});
+client.on('message',(data)=>{
+    var d = new Date();
+    process.stdout.write("\r"+d.getTime().toString()+" "+data.toString()+"                                 \n");
+    var d = new Date();
+    process.stdout.write(d.getTime().toString()+" ["+myArgs[1]+"-->]: ");});
+
+
+
---
+++ Server.js
@@ -0,0 +1,49 @@
+import {WebSocketServer} from "ws";
+
+var myArgs = process.argv.slice(2)
+
+const wss = new WebSocketServer({port: 7071, ip: myArgs[0]});
+const router = new Map();
+
+
+wss.on('connection', function connection(ws) {
+    let gotname = false
+    ws.on('message', function message(data) {
+        let from = [...router.entries()]
+            .filter(({1: v}) => v == ws)
+            .map(([k]) => k);
+
+        let pieces = data.toString().split(" ")
+        let cimzett = pieces[0]
+        if (!gotname) {
+            router.set(cimzett, ws)
+            gotname = true
+            if(router.has(myArgs[1])){
+                router.get(myArgs[1]).send("Name registered: "+cimzett)
+            }
+            console.log("Name registered: "+cimzett)
+        } else {
+            let names = cimzett.split(",")
+            pieces.shift()
+            let msg = pieces.join(" ")
+            if (router.has(myArgs[1])) {
+                router.get(myArgs[1]).send("[" + from + "-->" + cimzett + "]: " + msg)
+            }
+            console.log("[" + from + "-->" + cimzett + "]: " + msg);
+            var sent = false
+            for (let name of names) {
+                if (router.has(name)) {
+                    if(name != myArgs[1]) {
+                        router.get(name).send("[" + from + "-->" + name + "]: " + msg);
+                    }
+                    sent = true;
+                }
+            }
+            if (!sent) {
+                ws.send("There was no valid target!")
+            }
+        }
+    });
+
+    ws.send('Requested connection active! Handheld terminal registered.');
+});
